f66baea44f3d3920f8db46b027af1bb1ddd00316
Baze Petrushev <b.petrushev@gmail.com>
2016-04-13 16:35:58 +0200

    Add entry sitemap

--- gen_sitemaps.py
+++ gen_sitemaps.py
@@ -5,10 +5,11 @@ from os.path import dirname
 from sqlalchemy.sql.expression import func
 from lxml.etree import Element, SubElement
 from lxml.etree import tostring as xml_to_string
+from werkzeug.urls import url_quote
 
+from mkopen.utils import uuid2b64
 from mkopen.db.mappers import sessionmaker
 from mkopen.db.models import Data
-from werkzeug.urls import url_quote
 
 
 SITEMAP_PATH = path_join(dirname(__file__), 'wsgi', 'static')
@@ -21,6 +22,10 @@ Session = sessionmaker(
 def main():
     session = Session()
 
+    gen_catalogs(session)
+    gen_entries(session)
+
+def gen_catalogs(session):
     doc = Element("urlset", {"xmlns":"http://www.sitemaps.org/schemas/sitemap/0.9"})
 
     len_ = func.array_length(Data.catalog_id, 1)
@@ -33,9 +38,26 @@ def main():
            'https://%s/search?catalog=%s' % (environ['OPENSHIFT_APP_DNS'],
                                              url_quote(catalog_param))
         SubElement(url_xml, 'changefreq').text = 'weekly'
-        SubElement(url_xml, 'priority').text = '0.9'
+        SubElement(url_xml, 'priority').text = '0.8'
 
     with open(path_join(SITEMAP_PATH, 'catalog.xml'), 'wb') as f:
+        f.write(xml_to_string(doc, encoding='utf-8', pretty_print=True))
+
+def gen_entries(session):
+    doc = Element("urlset", {"xmlns":"http://www.sitemaps.org/schemas/sitemap/0.9"})
+
+    entries = session.query(Data.id).all()
+    for entry in entries:
+        hash = uuid2b64(entry.id)
+
+        url_xml = SubElement(doc, "url")
+        SubElement(url_xml, 'loc').text = \
+           'https://%s/entry/%s' % (environ['OPENSHIFT_APP_DNS'],
+                                    hash)
+        SubElement(url_xml, 'changefreq').text = 'weekly'
+        SubElement(url_xml, 'priority').text = '0.9'
+
+    with open(path_join(SITEMAP_PATH, 'entries.xml'), 'wb') as f:
         f.write(xml_to_string(doc, encoding='utf-8', pretty_print=True))
 
 
