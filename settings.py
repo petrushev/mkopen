a63e0ce5c5929d8723b3e560465afb8ed07c748a
Baze Petrushev <b.petrushev@gmail.com>
2017-10-01 19:16:45 +0200

    Adapt to non-openshift setup

---
+++ settings.py
@@ -0,0 +1,6 @@
+env = {
+    'GOOGLE_WEBMASTER': '',
+    'OPENSHIFT_POSTGRESQL_DB_URL': '',
+    'PGDATABASE': 'mkopen',
+    'OPENSHIFT_APP_DNS': '',
+}
--- wsgi.py
+++ wsgi.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python
 import os
 
-virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
-virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
-try:
-    execfile(virtualenv, dict(__file__=virtualenv))
-except IOError:
-    pass
-#
-# IMPORTANT: Put any additional includes below this line.  If placed above this
-# line, it's possible required libraries won't be in your searchable path
-#
+from setings import env
+
+for k, v in env:
+    os.environ[k] = v
+
 
 from mkopen.app import create_app
 
